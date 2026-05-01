f02b0bf6268b7041b376fe597e27f1c1ed8a7751
akashbagath48-debug <akashbagath48@gmail.com>
2026-05-01 17:35:47 +0530

    Moved email credentials to environment variables

--- app.py
+++ app.py
@@ -16,9 +16,8 @@ app.config['MAIL_PORT'] = 587
 
 app.config['MAIL_USE_TLS'] = True
 
-app.config['MAIL_USERNAME'] = 'akashbagath48@gmail.com'
-
-app.config['MAIL_PASSWORD'] = 'qxux zqjn jmax vohw'
+app.config['MAIL_USERNAME'] = os.environ.get('MAIL_USERNAME')
+app.config['MAIL_PASSWORD'] = os.environ.get('MAIL_PASSWORD')
 
 mail = Mail(app)
 
